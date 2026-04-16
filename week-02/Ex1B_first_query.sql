a0e5e9c74d945d04a7b821f1e5e9069f028c6ddd
Jimmy Juarez <jimmyjuarez2021@gmail.com>
2026-04-16 14:53:28 -0500

    commit

--- week-02/Ex1B_first_query.sql
+++ week-02/Ex1B_first_query.sql
@@ -1,3 +1,6 @@
 /* this data base i used to collect an asusmed Movie rental store to collect data on movies
 and customers, along side keeping track of their payments and staff.
 */
+USE sakila;
+SELECT * FROM actor; -- Retrieved 200 records
+SELECT * FROM film; -- Retrieved 1000 records
