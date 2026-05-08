e65710a365e5f7b15cb6a9fe8c87e222d96acaef
Jimmy Juarez <jimmyjuarez2021@gmail.com>
2026-05-08 00:06:02 -0500

    variable_example

---
+++ week-05/Ex1B_variable_examples.py
@@ -0,0 +1,43 @@
+customer_id = 1
+# I wrote 0 assuming were making he first customer
+customer_name = 'Jimmy Juarez'
+# I wrote a full name assuming we want first tHEN LAST
+customer_dob = '10-09-2001'
+# this is the common birthday convention in the USA
+customer_gender = 'Male'
+# I pthe full gener 'Male' instead of 'M' assuming we are using a conventional form
+drivers_license_num = 'J503-4032-5963'
+# I wrote a random drivers license number following convention
+auto_policy_number = 'PA-8439135-02'
+# I wrote a random auto policy number following convention
+
+
+'''
+a) What is the full list of reserved words that can’t be used for variable names?
+
+
+False def if raise
+None del import	return
+True elif in try
+and	else is	while
+as	except	lambda	with
+assert finally nonlocal yield
+break for not
+class form or
+continue global	pass
+
+b) Pick 5 of these words and review the explanation for how it is used as a keyword in
+Python. Add these 5 definitions as # comments to your exercise document. Put ^^
+around any terms that you are not familiar with.
+
+False - It is a boolean operator that represents the opposite of True.
+
+def - The def function is used to define a function or a method in Python.
+
+if - An if statement is used to make a conditional statement. If the condition is True, then some action is performed.
+
+None - There is no null value in Python. The None is an object that represents the absence of 
+a value. It is like an empty object.
+
+import - This statement is used to import modules to the project.
+'''
