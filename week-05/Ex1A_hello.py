86e5a22e0a25272b6e64d23d2f3f9b43ca82525b
Jimmy Juarez <jimmyjuarez2021@gmail.com>
2026-05-07 22:58:27 -0500

    dollars and cents

--- week-05/Ex1A_hello.py
+++ week-05/Ex1A_hello.py
@@ -1,1 +1,21 @@
-print('Hello World')
+print('Hello World!')
+
+message = 'Hello World!'
+
+print(message)
+
+# Displaying dollars and cents
+
+dollars = 3
+cents = .50
+
+print(dollars + cents)
+
+cents += .25
+
+print(dollars + cents)
+
+d_str = '3 dollars'
+c_str = '50 cents'
+
+print(f'{d_str} {c_str}')
