278252701fbca6db0d8c2fd548553eeda5cd418f
Jimmy Juarez <jimmyjuarez2021@gmail.com>
2026-05-11 04:08:15 -0500

    input modification on tips and f to c

---
+++ week-05/Ex2A_MathScripts/f_to_c.py
@@ -0,0 +1,3 @@
+fahrenheit = 72
+celsius = (fahrenheit - 32) * 5 / 9
+print(f"{fahrenheit}°F is {celsius:.2f}°C")
--- week-05/Ex2A_MathScripts/tip_amount.py
+++ week-05/Ex2A_MathScripts/tip_amount.py
@@ -1,5 +1,10 @@
-bill = 85.50
-tip_percent = 0.20
+bill = float(input("What is the restaurant bill amount? $"))
+tip_percent = float(input("What tip percentage do you want to give? (e.g. 0.20 for 20%) "))
+
 tip_amount = bill * tip_percent
 
-print(f"The tip on a ${bill} restaurant bill is ${tip_amount}")
+print(f"The tip on a ${bill} restaurant bill is ${tip_amount:.2f}")
+
+# Pitfalls:
+# input() returns string so must use float()
+# crashes if user enters letters
