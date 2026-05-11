3739f6212d50ff25e9023ad6d93060584cbb7b62
Jimmy Juarez <jimmyjuarez2021@gmail.com>
2026-05-11 03:32:05 -0500

    net_worth

---
+++ week-05/Ex2A_MathScripts/net_worth.py
@@ -0,0 +1,6 @@
+assets = 10000
+debt = 3000
+
+print(f'Your total assets are ${assets:,.2f}')
+print(f'Your total debts are ${debt:,.2f}')
+print(f'Your net worth is ${(int(assets) - int(debt)):,.2f}')
